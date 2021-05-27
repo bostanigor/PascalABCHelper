ea7c89e9abcec229882582480b99ea6a2cbd7ffb
Bostan Igor <bostanigor@gmail.com>
2021-05-27 15:37:48 +0300

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :student, optional: true
+  has_one :student, required: false
 end
